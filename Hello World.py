c928cf2190f95551e9c08cd71b6652a6ed0ab9de
Sana Zafar <98575007+sanzds@users.noreply.github.com>
2022-01-29 16:19:30 +0500

    Numbering Comments

--- Hello World.py
+++ Hello World.py
@@ -1,12 +1,12 @@
 #Handling Software Versions in GIT GUI
 
-#First Version of Software
+#1st Version of Software
 print("Hello World Software")
 
-#Second Version of Software
+#2nd Version of Software
 print("Changes in 1st Version of Hello World Software")
 
-#Third Version of Software
+#3rd Version of Software
 print("Changes in 2nd Version of Hello World Software")
 
 #5th Version of Software
