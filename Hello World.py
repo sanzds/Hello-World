f28c7368f509aa6c64d51c74078b7186333f299a
Sana Zafar <98575007+sanzds@users.noreply.github.com>
2022-01-29 16:12:05 +0500

    Add an existing file.

---
+++ Hello World.py
@@ -0,0 +1,13 @@
+#Handling Software Versions in GIT GUI
+
+#First Version of Software
+print("Hello World Software")
+
+#Second Version of Software
+print("Changes in 1st Version of Hello World Software")
+
+#Third Version of Software
+print("Changes in 2nd Version of Hello World Software")
+
+#5th Version of Software
+print("Changes in 3nd Version of Hello World Software")
